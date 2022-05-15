b071edb0c263fbb7bfe82db60bcd8b830840b594
chrism-48 <moore4020@gmail.com>
2022-05-14 22:23:19 -0400

    changed animations

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 let myContainer = document.querySelector('.container0');
 let myWelcome = document.querySelector('.welcome');
 let myPicture = document.querySelector('#me');
-
+let myContainer2 = document.querySelector('.container2');
+let myParagraph = document.querySelector('p');
 
 anime({
     targets: [myContainer,myWelcome],  
     translateX: {       
         value:[-250,0],
-        opacity:1,
         duration:2300,       
         },   
         rotate: {
@@ -20,7 +20,7 @@ anime({
             value:5.0,
             duration:3000,           
         },
-        delay:1000,       
+        delay:1000,          
 });
 
 
@@ -28,9 +28,18 @@ anime({
     targets: [myPicture],  
     scale: {
         value: [0,1], 
-        duration:3000, 
+        duration:3200, 
         easing:'easeInOutExpo',          
     },
     delay:1000,
        
 });
+
+
+// anime ({
+//     targets:[myContainer2, myParagraph],
+//     translateX: {
+//         value: [2002, 0], 
+//         duration:3200,       
+//     },
+// });
