d43feb15b0aa7882cba208feadb1e909ae796fb9
chrism-48 <moore4020@gmail.com>
2022-05-14 21:40:37 -0400

    add animation to image

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 
 let myContainer = document.querySelector('.container0');
 let myWelcome = document.querySelector('.welcome');
+let myPicture = document.querySelector('#me');
+
 
 anime({
     targets: [myContainer,myWelcome],  
     translateX: {       
         value:[-250,0],
-        opacity:0,
-        duration:2300,
-        
+        opacity:1,
+        duration:2300,       
         },   
         rotate: {
         value:180,
@@ -16,13 +17,20 @@ anime({
         easing: 'easeInOutSine',   
         }, 
         scale: {
-            value:5.5,
+            value:5.0,
             duration:3000,           
         },
-        delay:1000,
-        
-        
+        delay:1000,       
 });
 
 
-
+anime({
+    targets: [myPicture],  
+    scale: {
+        value: [0,1], 
+        duration:3000, 
+        easing:'easeInOutExpo',          
+    },
+    delay:1000,
+       
+});
